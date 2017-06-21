22565b93984c26b4e94aaff127e69a4f7a7cd69c
Carlos Gonzales <carlos.rgn@gmail.com>
2017-06-20 18:32:59 -0600

    handle non object key results

--- src/main.js
+++ src/main.js
@@ -3,14 +3,17 @@ const sortKeys = require('sort-keys')
 module.exports = function (array, keySelector, elementSelector = null) {
   const keys = {}
   const map = new Map()
-  for (let item of array) {
-    const key = sortKeys(keySelector(item), {deep: true})
+  array.forEach((item, index) => {
+    let key = keySelector(item, index)
+    if (typeof key === 'object') {
+      key = sortKeys(key, {deep: true})
+    }
     const str = JSON.stringify(key)
     if (typeof keys[str] === 'undefined') {
       keys[str] = key
       map.set(key, [])
     }
     map.get(keys[str]).push(elementSelector !== null ? elementSelector(item) : item)
-  }
+  })
   return map
 }
